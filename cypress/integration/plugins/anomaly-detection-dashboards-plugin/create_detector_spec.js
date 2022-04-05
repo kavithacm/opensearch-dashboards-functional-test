d5926b31a9f259b71396759aff0985450795108f
Kawika Avilla <kavilla414@gmail.com>
2022-04-05 08:21:28 +0000

    stop detector
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- cypress/integration/plugins/anomaly-detection-dashboards-plugin/create_detector_spec.js
+++ cypress/integration/plugins/anomaly-detection-dashboards-plugin/create_detector_spec.js
@@ -87,6 +87,7 @@ context('Create detector workflow', () => {
         .invoke('text')
         .then((detectorId) => {
           cy.log('Deleting detector with ID: ' + detectorId);
+          cy.stopDetector(detectorId);
           cy.deleteDetector(detectorId);
         });
     });
