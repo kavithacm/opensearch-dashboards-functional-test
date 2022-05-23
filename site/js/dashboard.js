b293a93696bd8e9f05974d931ac3f6b928b3089b
Kawika Avilla <kavilla414@gmail.com>
2022-05-23 22:51:47 +0000

    add pages
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

---
+++ site/js/dashboard.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-unused-vars
+function getTestResults() {
+  const version = document.getElementById('version').value;
+  const testJobName = document.getElementById('testJobName').value;
+  const buildNumber = document.getElementById('buildNumber').value;
+  const platform = document.getElementById('platform').value;
+  const arch = document.getElementById('arch').value;
+  const type = document.getElementById('type').value;
+  const securityEnabled = document.getElementById('security').checked;
+  const testResultsUrl =
+    'https://ci.opensearch.org/ci/dbc/' +
+    `${testJobName}/` +
+    `${version}/` +
+    `${buildNumber}/` +
+    `${platform}/` +
+    `${arch}/` +
+    `${type}/` +
+    'test-results/1/integ-test/functionalTestDashboards/' +
+    `${securityEnabled ? 'with-security' : 'without-security'}/` +
+    'test-results/stdout.txt';
+
+  document.getElementById('rawTestResults').src = testResultsUrl;
+  const rawTestResultsIframe =
+    document.getElementById('rawTestResults').contentWindow.document.body
+      .innerHTML;
+  document.getElementById('testResults').src =
+    decodeURIComponent(rawTestResultsIframe);
+  // fetch(testResultsUrl, { mode: 'no-cors' }).then((response) => {
+  //   response.text().then((data) => {
+  //     document.getElementById('testResults').innerHTML = data;
+  //   });
+  // });
+  // document.getElementById('absoluteUrl').innerHTML = testResultsUrl;
+}
+
+// https://ci.opensearch.org/ci/dbc/Playground/avillk-integ-test-opensearch-dashboards/2.0.0-rc1/322/linux/x64/tar/test-results/1/integ-test/functionalTestDashboards/with-security/test-results/stdout.txt
